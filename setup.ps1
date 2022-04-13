900fad836597f1bdabec4e727026d2b07f7faea1
YuryOliveira <39533089+YuryOliveira@users.noreply.github.com>
2022-04-13 09:14:14 -0300

    Create setup.ps1

---
+++ setup.ps1
@@ -0,0 +1,19 @@
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+(New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1')
+
+$SoftwareList = "https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/softwares.json"
+
+(Invoke-WebRequest $SoftwareList -UseBasicParsing).Content | ConvertFrom-Json | ForEach-Object {
+    
+    if(!([string]::IsNullOrEmpty($_.Args))) 
+    {
+        Write-Host "instalando $($_.Name) $($_.Args) `n"
+        #choco install $_.Name -Param $_.Args --y --force
+    }
+    else
+    {
+        Write-Host "instalando $($_.Name) `n"
+        #choco install $_.Name
+    }
+}
