05312bf802247f677f2de72b4915d00e54231944
YuryOliveira <39533089+YuryOliveira@users.noreply.github.com>
2022-04-14 16:07:25 -0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -29,7 +29,33 @@ if($applist.office -eq $True)
 
     Start-Process "$env:TMP\officedeploymenttool.exe" -ArgumentList "/quiet /extract:`"$env:TMP\officedeploymenttool`"" -Wait
 
-    Start-Process "$env:TMP\officedeploymenttool\Setup.exe" -ArgumentList "/configure https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/deployment_office2021.xml"
+    Start-Process "$env:TMP\officedeploymenttool\Setup.exe" -ArgumentList "/configure https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/deployment_office2021.xml" -wait
+}
+
+if($applist.actwinoffice -eq $True)
+{
+    $url  = ((Invoke-RestMethod -Method GET -Uri "https://api.github.com/repos/abbodi1406/KMS_VL_ALL_AIO/releases")[0].assets | Where-Object name -like "KMS_VL_ALL_AIO-*.7z" ).browser_download_url
+    $file = ("$url" -split '/')[-1]
+    $pass = "2022"
+    
+    try 
+    {
+        (New-Object net.webclient).Downloadfile($url, "$env:TMP\$file")
+    }
+    catch 
+    {
+        (New-Object net.webclient).Downloadfile($url, "$env:TMP\$file")
+    }
+
+    if(Test-Path "$env:TMP\$file")
+    {
+        Start-Process "$env:ProgramFiles\7-Zip\7z.exe" -ArgumentList "x `"$env:TMP\$file`" -o`"$env:TMP`" -p`"$pass`"" -NoNewWindow -Wait
+        Start-Process "$env:TMP\KMS_VL_ALL_AIO.cmd" -ArgumentList "/o /w /a" -NoNewWindow -Wait
+        if([string]::IsNullOrEmpty($(Get-Process cmd -ea 0)))
+        {
+            remove-item "$env:TMP\KMS_VL_ALL_AIO*" -Force
+        }
+    }
 }
 
 remove-item C:\ProgramData\chocolatey -Recurse -Force
