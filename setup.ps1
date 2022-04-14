ff184f55c7c642072e21d768adb0b290d27fa827
YuryOliveira <39533089+YuryOliveira@users.noreply.github.com>
2022-04-14 11:20:48 -0300

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -4,9 +4,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 
 invoke-expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-$applist = (Invoke-WebRequest "https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/applist/applist.json" -UseBasicParsing).Content | ConvertFrom-Json
+$applist = (Invoke-WebRequest "https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/applist.json" -UseBasicParsing).Content | ConvertFrom-Json
 
-$applist | ForEach-Object {
+$applist.chocolatey | ForEach-Object {
     
     if(!([string]::IsNullOrEmpty($_.Args))) 
     {
@@ -17,3 +17,19 @@ $applist | ForEach-Object {
         choco install $_.Name --y --force
     }
 }
+
+#Install Office 2021 LTSC
+if($applist.office -eq $True)
+{
+    $file = "$env:TMP\officedeploymenttool.exe"
+    $Uri  = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
+    $url  = ((Invoke-WebRequest $Uri -UseBasicParsing ).Links | Where-Object {$_.outerHTML -match "click here to download manually"}).href
+    
+    (New-Object net.webclient).Downloadfile($url, $file)
+
+    Start-Process "$env:TMP\officedeploymenttool.exe" -ArgumentList "/quiet /extract:`"$env:TMP\officedeploymenttool`"" -Wait
+
+    Start-Process "$env:TMP\officedeploymenttool\Setup.exe" -ArgumentList "/configure https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/deployment_office2021.xml"
+}
+
+remove-item C:\ProgramData\chocolatey -Recurse -Force
