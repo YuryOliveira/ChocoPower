0ce757aa3b3ab2fa21b1f895fab3b82146dcd8d7
YuryOliveira <yurypoliveira@gmail.com>
2022-04-27 13:41:06 -0300

    Update

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,7 @@
 <#
 .EXAMPLE
-powershell -command "invoke-expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/setup.ps1'))"
+powershell -Verb RunAs -command "invoke-expression ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/setup.ps1'))"
 #>
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        $Command = $MyInvocation.UnboundArguments
-        Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
-        pause
- }
-}
 
 Set-ExecutionPolicy Bypass -Scope Process -Force
 
