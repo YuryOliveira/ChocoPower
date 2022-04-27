10d48dc9c855bb9b5cdbae55f18303a17804ba5d
YuryOliveira <yurypoliveira@gmail.com>
2022-04-27 13:39:29 -0300

    Update

--- setup.ps1
+++ setup.ps1
@@ -6,7 +6,7 @@ if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
         $Command = $MyInvocation.UnboundArguments
         Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $Command
-        Exit
+        pause
  }
 }
 
