c321808f3a13041bd92b493a24c5a6ddc5995831
YuryOliveira <yurypoliveira@gmail.com>
2022-04-28 10:26:42 -0300

    update

--- setup.ps1
+++ setup.ps1
@@ -5,20 +5,7 @@ powershell -command "invoke-expression ((New-Object System.Net.WebClient).Downlo
 Set-ExecutionPolicy Bypass -Scope Process -Force
 [Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
 
-$MyInvocation | fl *
-$MyInvocation.BoundParameters | fl *
-$MyInvocation.UnboundArguments | fl *
-pause
-
-if (-Not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')) {
-    if ([int](Get-CimInstance -Class Win32_OperatingSystem | Select-Object -ExpandProperty BuildNumber) -ge 6000) {
-        Start-Process -FilePath PowerShell.exe -Verb RunAs -ArgumentList $MyInvocation.UnboundArguments
-        $MyInvocation | fl *
-        $MyInvocation.BoundParameters | fl *
-        $MyInvocation.UnboundArguments | fl *
-        pause
- }
-}
+$temp = (New-item "C:\$(New-Guid)" -ItemType Directory -Force -Confirm:$false).FullName
 
 invoke-expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
@@ -62,45 +49,40 @@ function Start_Install
 
 function Custom_Install($name,$arg)
 {
-    #Install Office 2021 LTSC
-    if($name -eq "office")
+    switch ($name)
     {
-        $file = "$env:TMP\officedeploymenttool.exe"
-        $Uri  = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
-        $url  = ((Invoke-WebRequest $Uri -UseBasicParsing ).Links | Where-Object {$_.outerHTML -match "click here to download manually"}).href
-    
-        (New-Object net.webclient).Downloadfile($url, $file)
-
-        Start-Process "$env:TMP\officedeploymenttool.exe" -ArgumentList "/quiet /extract:`"$env:TMP\officedeploymenttool`"" -NoNewWindow -Wait
-
-        Start-Process "$env:TMP\officedeploymenttool\Setup.exe" -ArgumentList "/configure https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/deployment_office2021.xml" -NoNewWindow -Wait
-    }
-
-    #Active Windows and Office
-    if($name -eq "actwinoffice")
-    {
-        $uri  = "https://api.github.com/repos/massgravel/Microsoft-Activation-Scripts/releases"
-        $zip  = "MAS_*.7z"
-        $url  = ((Invoke-RestMethod -Method GET -Uri $uri)[0].assets | Where-Object name -like $zip ).browser_download_url
-        $file = ("$url" -split '/')[-1]
-        $pass = "1234"
-        $temp = (New-item "C:\temp" -ItemType Directory -Force -Confirm:$false).FullName
-        try 
+        "office" 
         {
-            (New-Object net.webclient).Downloadfile($url, "$temp\$file")
+            #Install Office 2021 LTSC
+            $file = "$env:TMP\officedeploymenttool.exe"
+            $Uri  = "https://www.microsoft.com/en-us/download/confirmation.aspx?id=49117"
+            $url  = ((Invoke-WebRequest $Uri -UseBasicParsing ).Links | Where-Object {$_.outerHTML -match "click here to download manually"}).href
+            (New-Object net.webclient).Downloadfile($url, $file)
+            Start-Process "$env:TMP\officedeploymenttool.exe" -ArgumentList "/quiet /extract:`"$env:TMP\officedeploymenttool`"" -NoNewWindow -Wait
+            Start-Process "$env:TMP\officedeploymenttool\Setup.exe" -ArgumentList "/configure https://raw.githubusercontent.com/YuryOliveira/ChocoPower/main/deployment_office2021.xml" -NoNewWindow -Wait
         }
-        catch 
+        "actwinoffice"
         {
-            (New-Object net.webclient).Downloadfile($url, "$temp\$file")
+            #Active Windows and Office
+            $uri  = "https://api.github.com/repos/massgravel/Microsoft-Activation-Scripts/releases"
+            $zip  = "MAS_*.7z"
+            $url  = ((Invoke-RestMethod -Method GET -Uri $uri)[0].assets | Where-Object name -like $zip ).browser_download_url
+            $file = ("$url" -split '/')[-1]
+            $pass = "1234"
+            try { 
+                    (New-Object net.webclient).Downloadfile($url, "$temp\$file")
+            }
+            catch {
+                    (New-Object net.webclient).Downloadfile($url, "$temp\$file")
+            }
+            if(Test-Path "$temp\$file") {
+                Start-Process "$env:ProgramData\chocolatey\bin\7z.exe" -ArgumentList "x `"$temp\$file`" -o`"$temp`" -p`"$pass`"" -NoNewWindow -Wait
+                Start-Process "$temp\MAS_*\Separate-Files-Version\Activators\HWID-KMS38_Activation\HWID_Activation.cmd" -ArgumentList "/a" -NoNewWindow -Wait #Activate Windows Digital License
+                Start-Process "$temp\MAS_*\Separate-Files-Version\Activators\Online_KMS_Activation\Activate.cmd" -ArgumentList "/o" -NoNewWindow -Wait #Activate Office 180 days
+                Start-Process "$temp\MAS_*\Separate-Files-Version\Activators\Online_KMS_Activation\Activate.cmd" -ArgumentList "/rt" -NoNewWindow -Wait #Create Renewal Task for Office
+            }
         }
-
-        if(Test-Path "$temp\$file")
-        {
-            Start-Process "$env:ProgramData\chocolatey\bin\7z.exe" -ArgumentList "x `"$temp\$file`" -o`"$temp`" -p`"$pass`"" -NoNewWindow -Wait
-            Start-Process "$temp\MAS_*\Separate-Files-Version\Activators\HWID-KMS38_Activation\HWID_Activation.cmd" -ArgumentList "/a" -NoNewWindow -Wait #Activate Windows Digital License
-            Start-Process "$temp\MAS_*\Separate-Files-Version\Activators\Online_KMS_Activation\Activate.cmd" -ArgumentList "/o" -NoNewWindow -Wait #Activate Office 180 days
-            Start-Process "$temp\MAS_*\Separate-Files-Version\Activators\Online_KMS_Activation\Activate.cmd" -ArgumentList "/rt" -NoNewWindow -Wait #Create Renewal Task for Office
-        }
+        Default {}
     }
 }
 
